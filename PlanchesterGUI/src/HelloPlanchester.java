29d2329b71625c9f8f80270c75b222610f77d818
i7Gamer <timorzipa@gmail.com>
2017-03-29 09:43:56 +0200

    gui added

--- PlanchesterGUI/src/HelloPlanchester.java
+++ PlanchesterGUI/src/HelloPlanchester.java
@@ -7,8 +7,8 @@ public class HelloPlanchester {
         System.out.println("Hello Team D!");
         System.out.println("Welcome");
         System.out.println("Hello Christina! Can you push me :D?");
-
     }
+
     //change2
     public static void helloTeamD() {
         System.out.println("Hello Team D 2.0!");
