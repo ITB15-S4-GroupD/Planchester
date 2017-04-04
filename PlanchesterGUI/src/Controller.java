f64a898edfba93c2227fa282e5141496547411c1
i7Gamer <timorzipa@gmail.com>
2017-04-04 09:00:50 +0200

    Navigation in Calendar now works, Some hardcoded events added

--- PlanchesterGUI/src/Controller.java
+++ PlanchesterGUI/src/Controller.java
@@ -1,10 +1,32 @@
 import javafx.fxml.FXML;
+import jfxtras.scene.control.agenda.Agenda;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Controller {
 
     @FXML
     private void selectionChanged() {
         System.out.println("selection changed");
+    }
+
+    @FXML
+    private void navigateOneWeekBackClicked() {
+        Agenda agenda = (Agenda) Main.scene.lookup("#agenda");
+
+        LocalDateTime displayedDate = agenda.getDisplayedLocalDateTime();
+
+        agenda.setDisplayedLocalDateTime(displayedDate.minus(7, ChronoUnit.DAYS));
+    }
+
+    @FXML
+    private void navigateOneWeekForwardClicked() {
+        Agenda agenda = (Agenda) Main.scene.lookup("#agenda");
+
+        LocalDateTime displayedDate = agenda.getDisplayedLocalDateTime();
+
+        agenda.setDisplayedLocalDateTime(displayedDate.plus(7, ChronoUnit.DAYS));
     }
 
 }
--- PlanchesterGUI/src/Main.java
+++ PlanchesterGUI/src/Main.java
@@ -1,16 +1,94 @@
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import jfxtras.scene.control.agenda.Agenda;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Main extends Application {
 
+    public static Scene scene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        scene = new Scene(root, 800, 600);
+        Agenda agenda = (Agenda) scene.lookup("#agenda");
+
+        Agenda.Appointment appointment = new Agenda.AppointmentImpl();
+        LocalDateTime start = LocalDateTime.of(2017,Month.APRIL,4,13,00);
+        LocalDateTime end = LocalDateTime.of(2017,Month.APRIL,4,16,00);
+        appointment.setStartLocalDateTime(start);
+        appointment.setEndLocalDateTime(end);
+        appointment.setDescription("Test 1");
+        appointment.setLocation("Raum 5");
+        appointment.setSummary("Title 1");
+
+        Agenda.Appointment appointment2 = new Agenda.AppointmentImpl();
+        LocalDateTime start2 = LocalDateTime.of(2017,Month.APRIL,3,12,00);
+        LocalDateTime end2 = LocalDateTime.of(2017,Month.APRIL,3,14,00);
+        appointment2.setStartLocalDateTime(start2);
+        appointment2.setEndLocalDateTime(end2);
+        appointment2.setDescription("Test 2");
+        appointment2.setLocation("Room 3");
+        appointment2.setSummary("Title 2");
+
+        agenda.appointments().add(appointment);
+        agenda.appointments().add(appointment2);
+
+        agenda.setAllowDragging(false);
+        agenda.setAllowResize(false);
+
+        agenda.localeProperty().set(Locale.GERMAN);
+
+        agenda.onMouseClickedProperty().set(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent arg0){
+                System.out.println("Property Changed");
+                ObservableList<Agenda.Appointment> appointments = agenda.selectedAppointments();
+                Agenda.Appointment appointment = appointments.get(0);
+
+                TextField textField = (TextField) scene.lookup("#name");
+                textField.setText(appointment.getSummary());
+
+                TextField textField2 = (TextField) scene.lookup("#description");
+                textField2.setText(appointment.getDescription());
+
+                /*
+                Alert alert;
+                alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Property Clicked");
+                alert.setHeaderText(appointment.getDescription());
+                alert.setContentText(appointment.getDescription());
+
+                alert.showAndWait();
+                */
+            }
+        });
+
         primaryStage.setTitle("Planchester");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
