14fcdd91f0da4dbf2ca4fb03dd1d99ac73732c5b
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2017-04-05 16:18:23 +0200

    new Packages added

---
+++ src/Domain/Test.java
@@ -0,0 +1,7 @@
+package Domain;
+
+/**
+ * Created by julia on 05.04.2017.
+ */
+public class Test {
+}
---
+++ src/Persistence/Test.java
@@ -0,0 +1,7 @@
+package Persistence;
+
+/**
+ * Created by julia on 05.04.2017.
+ */
+public class Test {
+}
