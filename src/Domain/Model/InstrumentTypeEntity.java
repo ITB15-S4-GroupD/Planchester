40779f2e036d5287e0cd63446dee8c2d93e2a5c1
hurrgadev <bernd.orler@students.fhv.at>
2017-04-06 13:59:29 +0200

    Hibernate first steps

---
+++ src/Domain/Model/InstrumentTypeEntity.java
@@ -0,0 +1,54 @@
+package Domain.Model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Bernd on 06.04.2017.
+ */
+@Entity
+@Table(name = "InstrumentType", schema = "sem4_team2", catalog = "")
+public class InstrumentTypeEntity {
+    private int instrumentTypeId;
+    private String instrumentType;
+
+    @Id
+    @Column(name = "instrumentTypeID")
+    public int getInstrumentTypeId() {
+        return instrumentTypeId;
+    }
+
+    public void setInstrumentTypeId(int instrumentTypeId) {
+        this.instrumentTypeId = instrumentTypeId;
+    }
+
+    @Basic
+    @Column(name = "instrumentType")
+    public String getInstrumentType() {
+        return instrumentType;
+    }
+
+    public void setInstrumentType(String instrumentType) {
+        this.instrumentType = instrumentType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InstrumentTypeEntity that = (InstrumentTypeEntity) o;
+
+        if (instrumentTypeId != that.instrumentTypeId) return false;
+        if (instrumentType != null ? !instrumentType.equals(that.instrumentType) : that.instrumentType != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = instrumentTypeId;
+        result = 31 * result + (instrumentType != null ? instrumentType.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/Domain/Model/PartTypeEntity.java
@@ -0,0 +1,53 @@
+package Domain.Model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Bernd on 06.04.2017.
+ */
+@Entity
+@Table(name = "PartType", schema = "sem4_team2", catalog = "")
+public class PartTypeEntity {
+    private int partTypeId;
+    private String partType;
+
+    @Id
+    @Column(name = "partTypeID")
+    public int getPartTypeId() {
+        return partTypeId;
+    }
+
+    public void setPartTypeId(int partTypeId) {
+        this.partTypeId = partTypeId;
+    }
+
+    @Basic
+    @Column(name = "partType")
+    public String getPartType() {
+        return partType;
+    }
+
+    public void setPartType(String partType) {
+        this.partType = partType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PartTypeEntity that = (PartTypeEntity) o;
+
+        if (partTypeId != that.partTypeId) return false;
+        if (partType != null ? !partType.equals(that.partType) : that.partType != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = partTypeId;
+        result = 31 * result + (partType != null ? partType.hashCode() : 0);
+        return result;
+    }
+}
