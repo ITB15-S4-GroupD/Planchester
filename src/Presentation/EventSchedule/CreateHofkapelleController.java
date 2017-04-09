0bf22bf296f773cf30f46bb8383152fdad4b7b9c
tscholy <christina.tschol@students.fhv.at>
2017-04-09 13:26:28 +0200

    ChoiceBox for new Duty at EventSchedule created. Forms for duty "Opera" and "Hofkapelle" created (Testversions).

---
+++ src/Presentation/EventSchedule/CreateHofkapelleController.java
@@ -0,0 +1,61 @@
+package Presentation.EventSchedule;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import jfxtras.scene.control.LocalTimePicker;
+
+import java.util.Optional;
+
+/**
+ * Created by Ina on 09.04.2017.
+ */
+public class CreateHofkapelleController {
+
+    @FXML
+    private TextField txtName;
+    @FXML private TextArea txtDescription;
+    @FXML private DatePicker pckDate;
+    @FXML private LocalTimePicker pckStartTime;
+    @FXML private LocalTimePicker pckEndTime;
+    @FXML private TextField txtLocation;
+    @FXML private ChoiceBox<String> choiceWork;
+    @FXML private TextField txtConductor;
+    @FXML private TextField txtPoints;
+
+    @FXML
+    public void initialize() {
+
+        /* IODO fill Works from DB into choiceWork
+                Fit fields to duty Hofkapelle (copied from opera)
+         */
+
+    }
+
+    @FXML
+    private void saveNewHofkapelle(){
+
+        //i: Test
+
+        String collectedData = "Event Name: " + txtName.getText() + "\n" +
+                "Description: " + txtDescription.getText() + "\n" +
+                "Date: " + pckDate.getValue() + "\n" +
+                "Start Time: " + pckStartTime.getLocalTime().toString() + "\n" +
+                "End Time: " + pckEndTime.getLocalTime().toString() + "\n" +
+                "Location: " + txtLocation.getText() + "\n" +
+                "Description: " + txtDescription.getText() + "\n" +
+                "Work: " + choiceWork.getValue() + "\n" +
+                "Conductor: " + txtConductor.getText() + "\n" +
+                "Points: " + txtPoints.getText() + "\n";
+
+
+        Alert dialog = new Alert(Alert.AlertType.CONFIRMATION);
+        dialog.setHeaderText( "Daten werden gespeichert: " );
+        dialog.setContentText(collectedData);
+        dialog.setResizable(true);
+        dialog.getDialogPane().setPrefSize(350, 500);
+        dialog.showAndWait();
+        final Optional<ButtonType> result = dialog.showAndWait();
+
+    }
+
+}
