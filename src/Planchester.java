26ccccf5890b3e13495f4d16323181a53f717b8f
i7Gamer <timorzipa@gmail.com>
2017-04-10 09:20:10 +0200

    user and password added to hibernate config

--- src/Planchester.java
+++ src/Planchester.java
@@ -1,13 +1,38 @@
 import Presentation.PlanchesterGUI;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class Planchester extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        PlanchesterGUI gui = new PlanchesterGUI();
-        gui.start(primaryStage);
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");// populates the data of the
+        // configuration file
+
+        // creating seession factory object
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        // creating session object
+        Session session = factory.openSession();
+
+        // creating transaction object
+        Transaction t = session.beginTransaction();
+
+        Query query = session.createQuery("FROM InstrumentEntity");
+        java.util.List list = query.list();
+        System.out.println(list);
+        t.commit();
+        session.close();
+
+        //PlanchesterGUI gui = new PlanchesterGUI();
+        //gui.start(primaryStage);
     }
 
     public static void main(String[] args) {
