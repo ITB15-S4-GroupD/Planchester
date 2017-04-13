7fb918ee45c2ec662774069acba054d43c4cc010
i7Gamer <timorzipa@gmail.com>
2017-04-13 11:44:41 +0200

    Application now closing without problems

--- src/Planchester.java
+++ src/Planchester.java
@@ -1,8 +1,11 @@
 import Domain.Model.InstrumentEntity;
 import Presentation.PlanchesterGUI;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -13,6 +16,13 @@ public class Planchester extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
         PlanchesterGUI gui = new PlanchesterGUI();
         gui.start(primaryStage);
 
