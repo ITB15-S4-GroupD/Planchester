02e7fe2665b98bc2379027088a90ac53be47ad69
i7Gamer <timorzipa@gmail.com>
2017-04-18 09:00:19 +0200

    New validation and confirm message removed

--- src/Presentation/EventSchedule/CreateOperaController.java
+++ src/Presentation/EventSchedule/CreateOperaController.java
@@ -37,97 +37,126 @@ public class CreateOperaController {
     @FXML
     public void initialize() {
         //TODO all: fill Works from DB into musicalWork
+        points.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    points.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
     }
 
     @FXML
     private void save() {
-        System.out.print("save clicked");
-
-        String warning = "";
-        boolean validate = true;
-
-        if(name.getText().isEmpty()) {
-            validate = false;
-            warning = warning + "Name missing\n";
+        if(name.getText().isEmpty()){
+            doAlert("Name missing\n");
+            name.requestFocus();
+            return;
         }
 
-        LocalDate date = this.date.getValue();
-        if(date == null || !date.isAfter(LocalDate.now())) {
-            validate = false;
-            warning = warning + "Date wrong\n";
+        if(description.getText().isEmpty()){
+            doAlert("Description missing\n");
+            description.requestFocus();
+            return;
+        }
+
+
+        LocalDate today = LocalDate.now();
+        if(date.getValue() == null || date.getValue().isBefore(today) ){
+            doAlert("Date wrong\n");
+            this.date.requestFocus();
+            return;
         }
 
         LocalTime start = startTime.getValue();
         LocalTime end = endTime.getValue();
-        if(start == null || end == null ||start.isAfter(end)) {
-            validate = false;
-            warning = warning + "Endtime ist not after starttime\n";
+
+        if(start == null || end == null) {
+            doAlert("Start or Endtime missing\n");
+            return;
         }
 
-        if(eventLocation.getText().isEmpty()) {
-            validate = false;
-            warning = warning + "Location missing\n";
+        if(start.isAfter(end) || start.equals(end)) {
+            doAlert("Endtime is not after starttime\n");
+            return;
         }
 
-        if(conductor.getText().isEmpty()) {
-            validate = false;
-            warning = warning + "Conductor missing\n";
+        if(date.getValue().equals(today) && start.isBefore(LocalTime.now())){
+            doAlert("Starttime lies in the past\n");
+            return;
         }
 
-        if(points.getText().isEmpty()) {
-            validate = false;
-            warning = warning + "Points missing\n";
+        if(eventLocation.getText().isEmpty()){
+            doAlert("Location missing\n");
+            eventLocation.requestFocus();
+            return;
         }
 
-        if(validate) {
-            // create object
-            EventDutyEntity eventDutyEntity = new EventDutyEntity();
-
-            // save text data
-            eventDutyEntity.setName(name.getText());
-            eventDutyEntity.setDescription(description.getText());
-            eventDutyEntity.setLocation(eventLocation.getText());
-            eventDutyEntity.setEventType(EventType.Opera.toString());
-            eventDutyEntity.setEventStatus(EventStatus.Unpublished.toString());
-            eventDutyEntity.setConductor(conductor.getText());
-
-            // save points
-            eventDutyEntity.setDefaultPoints(Double.parseDouble(points.getText()));
-
-            // save time
-            Timestamp startTimestamp = Timestamp.valueOf(LocalDateTime.of(date, startTime.getValue()));
-            Timestamp endTimestamp = Timestamp.valueOf(LocalDateTime.of(date, endTime.getValue()));
-            eventDutyEntity.setStarttime(startTimestamp);
-            eventDutyEntity.setEndtime(endTimestamp);
-
-            // save object
-            EventDutyModel eventDuty = new EventDutyModel(eventDutyEntity);
-            EventSchedule.createOpera(eventDuty);
-
-            // add event to agenda
-            EventScheduleController.addEventDutyToGUI(eventDuty);
-
-            // set agenda view to week of created event
-            EventScheduleController.setDisplayedLocalDateTime(eventDuty.getEventDuty().getStarttime().toLocalDateTime());
-
-            // remove content of sidebar
-            EventScheduleController.resetSideContent();
-
-            Alert information = new Alert(Alert.AlertType.INFORMATION);
-            information.setHeaderText("Event saved");
-            information.setContentText("Event saved");
-            information.setResizable(false);
-            information.getDialogPane().setPrefSize(350, 500);
-            information.showAndWait();
-
-        }else {
-            Alert problem = new Alert(Alert.AlertType.ERROR);
-            problem.setHeaderText("Error");
-            problem.setContentText(warning);
-            problem.setResizable(false);
-            problem.getDialogPane().setPrefSize(350, 500);
-            problem.showAndWait();
+        if(conductor.getText().isEmpty()){
+            doAlert("Conductor missing\n");
+            conductor.requestFocus();
+            return;
         }
+
+        if(points.getText().isEmpty()){
+            doAlert("Points missing\n");
+            points.requestFocus();
+            return;
+        }
+
+
+
+        // create object
+        EventDutyEntity eventDutyEntity = new EventDutyEntity();
+
+        // save text data
+        eventDutyEntity.setName(name.getText());
+        eventDutyEntity.setDescription(description.getText());
+        eventDutyEntity.setLocation(eventLocation.getText());
+        eventDutyEntity.setEventType(EventType.Opera.toString());
+        eventDutyEntity.setEventStatus(EventStatus.Unpublished.toString());
+        eventDutyEntity.setConductor(conductor.getText());
+
+        // save points
+        eventDutyEntity.setDefaultPoints(Double.parseDouble(points.getText()));
+
+        // save time
+        Timestamp startTimestamp = Timestamp.valueOf(LocalDateTime.of(date.getValue(), startTime.getValue()));
+        Timestamp endTimestamp = Timestamp.valueOf(LocalDateTime.of(date.getValue(), endTime.getValue()));
+        eventDutyEntity.setStarttime(startTimestamp);
+        eventDutyEntity.setEndtime(endTimestamp);
+
+        // save object
+        EventDutyModel eventDuty = new EventDutyModel(eventDutyEntity);
+        EventSchedule.createOpera(eventDuty);
+
+        // add event to agenda
+        EventScheduleController.addEventDutyToGUI(eventDuty);
+
+        // set agenda view to week of created event
+        EventScheduleController.setDisplayedLocalDateTime(eventDuty.getEventDuty().getStarttime().toLocalDateTime());
+
+        // remove content of sidebar
+        EventScheduleController.resetSideContent();
+
+        /*
+        Alert information = new Alert(Alert.AlertType.INFORMATION);
+        information.setHeaderText("Event saved");
+        information.setContentText("Event saved");
+        information.setResizable(false);
+        information.getDialogPane().setPrefSize(350, 500);
+        information.showAndWait();
+        */
+    }
+
+    private void doAlert(String warning){
+        Alert problem = new Alert(Alert.AlertType.ERROR);
+        problem.setHeaderText("Error");
+        problem.setContentText(warning);
+        problem.setResizable(true);
+        problem.getDialogPane().setPrefSize(350, 200);
+        problem.showAndWait();
     }
 
     @FXML
@@ -141,6 +170,7 @@ public class CreateOperaController {
                 return false;
             }
         }
+
         // remove content of sidebar
         EventScheduleController.resetSideContent();
         return true;
