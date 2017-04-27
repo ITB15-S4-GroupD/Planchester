653968d71ac3cf3202839b92ac255998bee6fde9
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2017-04-27 15:12:23 +0200

    confirmation message outsourced...

--- src/Presentation/EventSchedule/CreateTourController.java
+++ src/Presentation/EventSchedule/CreateTourController.java
@@ -65,10 +65,9 @@ public class CreateTourController {
     public boolean cancel() {
         if(!name.getText().isEmpty() || !description.getText().isEmpty() || startDate.getValue() != null
                 || endDate.getValue() != null || !eventLocation.getText().isEmpty() || !conductor.getText().isEmpty() || !points.getText().isEmpty()) {
-            Alert confirmationAlterMessage = new Alert(Alert.AlertType.CONFIRMATION, PlanchesterMessages.DISCARD_CHANGES, ButtonType.YES, ButtonType.NO);
-            confirmationAlterMessage.showAndWait();
 
-            if (confirmationAlterMessage.getResult() == ButtonType.NO) {
+            ButtonType answer = MessageHelper.showConfirmationMessage(PlanchesterMessages.DISCARD_CHANGES);
+            if(ButtonType.NO.equals(answer)) {
                 return false;
             }
         }
--- src/Utils/MessageHelper.java
+++ src/Utils/MessageHelper.java
@@ -1,6 +1,7 @@
 package Utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 /**
  * Created by julia on 27.04.2017.
@@ -11,5 +12,17 @@ public class MessageHelper {
         alert.setTitle("Planchester Error");
         alert.setHeaderText(errormessage);
         alert.showAndWait();
+    }
+
+    public static ButtonType showConfirmationMessage(String confirmationMessage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Planchester Message");
+        alert.setHeaderText(confirmationMessage);
+        alert.getButtonTypes().clear();
+        alert.getButtonTypes().add(ButtonType.YES);
+        alert.getButtonTypes().add(ButtonType.NO);
+        alert.showAndWait();
+
+        return alert.getResult();
     }
 }
