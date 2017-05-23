ce0e946ac40e4d0866b37d9c285279c007f25824
i7Gamer <timorzipa@gmail.com>
2017-05-23 11:35:33 +0200

    fixed bugs and added checking if section_leader has been assigned to event

--- src/Application/PublishDutyRoster.java
+++ src/Application/PublishDutyRoster.java
@@ -4,6 +4,8 @@ import Persistence.Entities.EventDutySectionDutyRosterEntity;
 import Persistence.Entities.SectionDutyRosterEntity;
 import Persistence.PersistanceFacade;
 import Utils.Enum.DutyRosterStatus;
+import Utils.MessageHelper;
+
 import java.time.Month;
 import java.time.Year;
 import java.util.List;
@@ -20,13 +22,17 @@ public class PublishDutyRoster {
                 p.getEventDutyByEventDuty().getStarttime().toLocalDateTime().getMonth().equals(month)
                 && p.getEventDutyByEventDuty().getStarttime().toLocalDateTime().getYear() == year.getValue()
                 && p.getSectionDutyRosterBySectionDutyRoster().getSectionType().equals(AccountAdministrationManager.getInstance().getSectionType().toString())
-                && p.getSectionDutyRosterBySectionDutyRoster().getDutyRosterStatus().equals(DutyRosterStatus.Unpublished)
+                && p.getSectionDutyRosterBySectionDutyRoster().getDutyRosterStatus().equals(DutyRosterStatus.Unpublished.toString())
         );
 
         if(!eventDutySectionDutyRosterEntities.isEmpty()) {
-            SectionDutyRosterEntity sectionDutyRosterEntity = eventDutySectionDutyRosterEntities.get(0).getSectionDutyRosterBySectionDutyRoster();
-            PersistanceFacade<SectionDutyRosterEntity> sectionDutyRosterEntityPersistanceFacade = new PersistanceFacade(SectionDutyRosterEntity.class);
-            sectionDutyRosterEntityPersistanceFacade.put(sectionDutyRosterEntity);
+            for(EventDutySectionDutyRosterEntity eventDutySectionDutyRosterEntity : eventDutySectionDutyRosterEntities) {
+                PersistanceFacade<SectionDutyRosterEntity> sectionDutyRosterEntityPersistanceFacade = new PersistanceFacade(SectionDutyRosterEntity.class);
+                SectionDutyRosterEntity sectionDutyRosterEntity = eventDutySectionDutyRosterEntity.getSectionDutyRosterBySectionDutyRoster();
+                sectionDutyRosterEntity.setDutyRosterStatus(DutyRosterStatus.Published.toString());
+                sectionDutyRosterEntityPersistanceFacade.put(sectionDutyRosterEntity);
+            }
+            MessageHelper.showInformationMessage("All events of the month " + month.toString().toLowerCase() + " have been published");
         }
     }
 }
